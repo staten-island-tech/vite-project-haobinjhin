8c3a331f3886a521439478738b1c57c3564c64db
haobinjhin <144480283+haobinjhin@users.noreply.github.com>
2024-11-04 12:26:03 -0500

    dfsdfsdfs

--- app/JS/items.js
+++ app/JS/items.js
@@ -1,66 +1,185 @@
-const games = [
+const games = [[
     {
-      name: "Elden Ring",
-      releaseDate: "2022-02-25",
-      price: 59.99,
-      sale: Math.random() * (0.60 - 0.10) + 0.10, // Random discount between 10% and 60%
-      platforms: ["PC", "PS4", "PS5", "Xbox One", "Xbox Series X/S"],
-      studio: "FromSoftware",
-      director: "Hidetaka Miyazaki"
+        "name": "Dark Souls",
+        "releaseDate": "2011-09-22",
+        "price": 39.99,
+        "sale": false,
+        "platforms": ["PC", "PS3", "Xbox 360"],
+        "studio": "FromSoftware",
+        "director": "Hidetaka Miyazaki"
     },
     {
-      name: "Sekiro: Shadows Die Twice",
-      releaseDate: "2019-03-22",
-      price: 59.99,
-      sale: Math.random() * (0.60 - 0.10) + 0.10,
-      platforms: ["PC", "PS4", "Xbox One"],
-      studio: "FromSoftware",
-      director: "Hidetaka Miyazaki"
+        "name": "Bloodborne",
+        "releaseDate": "2015-03-24",
+        "price": 59.99,
+        "sale": true,
+        "platforms": ["PS4"],
+        "studio": "FromSoftware",
+        "director": "Hidetaka Miyazaki"
     },
     {
-      name: "Hollow Knight",
-      releaseDate: "2017-02-24",
-      price: 14.99,
-      sale: Math.random() * (0.60 - 0.10) + 0.10,
-      platforms: ["PC", "PS4", "Xbox One", "Nintendo Switch"],
-      studio: "Team Cherry",
-      director: "Ari Gibson"
+        "name": "Sekiro: Shadows Die Twice",
+        "releaseDate": "2019-03-22",
+        "price": 59.99,
+        "sale": false,
+        "platforms": ["PC", "PS4", "Xbox One"],
+        "studio": "FromSoftware",
+        "director": "Hidetaka Miyazaki"
     },
     {
-      name: "Bloodborne",
-      releaseDate: "2015-03-24",
-      price: 19.99,
-      sale: Math.random() * (0.60 - 0.10) + 0.10,
-      platforms: ["PS4"],
-      studio: "FromSoftware",
-      director: "Hidetaka Miyazaki"
+        "name": "Elden Ring",
+        "releaseDate": "2022-02-25",
+        "price": 59.99,
+        "sale": true,
+        "platforms": ["PC", "PS4", "PS5", "Xbox One", "Xbox Series X/S"],
+        "studio": "FromSoftware",
+        "director": "Hidetaka Miyazaki"
     },
     {
-      name: "Dark Souls",
-      releaseDate: "2011-09-22",
-      price: 39.99,
-      sale: Math.random() * (0.60 - 0.10) + 0.10,
-      platforms: ["PC", "PS3", "Xbox 360"],
-      studio: "FromSoftware",
-      director: "Hidetaka Miyazaki"
+        "name": "Assassin's Creed II",
+        "releaseDate": "2009-11-17",
+        "price": 39.99,
+        "sale": false,
+        "platforms": ["PC", "PS3", "Xbox 360"],
+        "studio": "Ubisoft",
+        "director": "Patrice Désilets"
     },
     {
-      name: "Dark Souls II",
-      releaseDate: "2014-03-11",
-      price: 39.99,
-      sale: Math.random() * (0.60 - 0.10) + 0.10,
-      platforms: ["PC", "PS3", "Xbox 360"],
-      studio: "FromSoftware",
-      director: "Tomohiro Shibuya, Yui Tanimura"
+        "name": "Far Cry 5",
+        "releaseDate": "2018-03-27",
+        "price": 59.99,
+        "sale": false,
+        "platforms": ["PC", "PS4", "Xbox One"],
+        "studio": "Ubisoft",
+        "director": "Dan Hay"
     },
     {
-      name: "Dark Souls III",
-      releaseDate: "2016-03-24",
-      price: 59.99,
-      sale: Math.random() * (0.60 - 0.10) + 0.10,
-      platforms: ["PC", "PS4", "Xbox One"],
-      studio: "FromSoftware",
-      director: "Hidetaka Miyazaki"
+        "name": "Watch Dogs",
+        "releaseDate": "2014-05-27",
+        "price": 49.99,
+        "sale": true,
+        "platforms": ["PC", "PS4", "Xbox One"],
+        "studio": "Ubisoft",
+        "director": "Jonathan Morin"
+    },
+    {
+        "name": "Mario Kart 8 Deluxe",
+        "releaseDate": "2017-04-28",
+        "price": 59.99,
+        "sale": false,
+        "platforms": ["Nintendo Switch"],
+        "studio": "Nintendo",
+        "director": "Kosuke Yabuki"
+    },
+    {
+        "name": "The Legend of Zelda: Breath of the Wild",
+        "releaseDate": "2017-03-03",
+        "price": 59.99,
+        "sale": false,
+        "platforms": ["Nintendo Switch", "Wii U"],
+        "studio": "Nintendo",
+        "director": "Hidemaro Fujibayashi"
+    },
+    {
+        "name": "Super Mario Odyssey",
+        "releaseDate": "2017-10-27",
+        "price": 59.99,
+        "sale": false,
+        "platforms": ["Nintendo Switch"],
+        "studio": "Nintendo",
+        "director": "Kenta Motokura"
+    },
+    {
+        "name": "Red Dead Redemption 2",
+        "releaseDate": "2018-10-26",
+        "price": 59.99,
+        "sale": false,
+        "platforms": ["PC", "PS4", "Xbox One"],
+        "studio": "Rockstar Games",
+        "director": "Rodney E. McCulloch"
+    },
+    {
+        "name": "GTA V",
+        "releaseDate": "2013-09-17",
+        "price": 29.99,
+        "sale": true,
+        "platforms": ["PC", "PS4", "Xbox One", "PS3", "Xbox 360"],
+        "studio": "Rockstar Games",
+        "director": "Leslie Benzies"
+    },
+    {
+        "name": "Max Payne 3",
+        "releaseDate": "2012-05-15",
+        "price": 39.99,
+        "sale": false,
+        "platforms": ["PC", "PS3", "Xbox 360"],
+        "studio": "Rockstar Games",
+        "director": "Sam Houser"
+    },
+    {
+        "name": "L.A. Noire",
+        "releaseDate": "2011-05-17",
+        "price": 49.99,
+        "sale": false,
+        "platforms": ["PC", "PS3", "Xbox 360"],
+        "studio": "Rockstar Games",
+        "director": "Brendan McNamara"
+    },
+    {
+        "name": "Assassin's Creed Odyssey",
+        "releaseDate": "2018-10-05",
+        "price": 59.99,
+        "sale": true,
+        "platforms": ["PC", "PS4", "Xbox One"],
+        "studio": "Ubisoft",
+        "director": "Scott Phillips"
+    },
+    {
+        "name": "Splinter Cell: Blacklist",
+        "releaseDate": "2013-08-20",
+        "price": 39.99,
+        "sale": false,
+        "platforms": ["PC", "PS3", "Xbox 360"],
+        "studio": "Ubisoft",
+        "director": "Patrick Redding"
+    },
+    {
+        "name": "Animal Crossing: New Horizons",
+        "releaseDate": "2020-03-20",
+        "price": 59.99,
+        "sale": true,
+        "platforms": ["Nintendo Switch"],
+        "studio": "Nintendo",
+        "director": "Aya Kyogoku"
+    },
+    {
+        "name": "Metroid Dread",
+        "releaseDate": "2021-10-08",
+        "price": 59.99,
+        "sale": false,
+        "platforms": ["Nintendo Switch"],
+        "studio": "Nintendo",
+        "director": "Yoshio Sakamoto"
+    },
+    {
+        "name": "Assassin's Creed Valhalla",
+        "releaseDate": "2020-11-10",
+        "price": 59.99,
+        "sale": false,
+        "platforms": ["PC", "PS4", "PS5", "Xbox One", "Xbox Series X/S"],
+        "studio": "Ubisoft",
+        "director": "Ashraf Ismail"
+    },
+    {
+        "name": "Nioh",
+        "releaseDate": "2017-02-07",
+        "price": 49.99,
+        "sale": true,
+        "platforms": ["PC", "PS4"],
+        "studio": "Team Ninja",
+        "director": "Fumihiko Yasuda"
     }
+]
+
   ];
   
