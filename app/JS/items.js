2583470958bc7f64d75fcfe0b2233ad6e83b1397
haobinjhin <144480283+haobinjhin@users.noreply.github.com>
2024-11-06 22:56:12 -0500

    dfgdfg

--- app/JS/items.js
+++ app/JS/items.js
@@ -1,186 +1,226 @@
-export const games = [[
+export const games = [
+    // FromSoftware
     {
-        "name": "Dark Souls",
-        "releaseDate": "2011-09-22",
-        "price": 39.99,
-        "sale": false,
-        "platforms": ["PC", "PS3", "Xbox 360"],
-        "studio": "FromSoftware",
-        "director": "Hidetaka Miyazaki"
+        name: "Dark Souls",
+        releaseDate: "2011-09-22",
+        price: 39.99,
+        platforms: ["PC", "PS3", "Xbox 360"],
+        studio: "FromSoftware",
+        director: "Hidetaka Miyazaki"
     },
     {
-        "name": "Bloodborne",
-        "releaseDate": "2015-03-24",
-        "price": 59.99,
-        "sale": true,
-        "platforms": ["PS4"],
-        "studio": "FromSoftware",
-        "director": "Hidetaka Miyazaki"
+        name: "Bloodborne",
+        releaseDate: "2015-03-24",
+        price: 59.99,
+        platforms: ["PS4"],
+        studio: "FromSoftware",
+        director: "Hidetaka Miyazaki"
     },
     {
-        "name": "Sekiro: Shadows Die Twice",
-        "releaseDate": "2019-03-22",
-        "price": 59.99,
-        "sale": false,
-        "platforms": ["PC", "PS4", "Xbox One"],
-        "studio": "FromSoftware",
-        "director": "Hidetaka Miyazaki"
+        name: "Sekiro: Shadows Die Twice",
+        releaseDate: "2019-03-22",
+        price: 59.99,
+        platforms: ["PC", "PS4", "Xbox One"],
+        studio: "FromSoftware",
+        director: "Hidetaka Miyazaki"
     },
     {
-        "name": "Elden Ring",
-        "releaseDate": "2022-02-25",
-        "price": 59.99,
-        "sale": true,
-        "platforms": ["PC", "PS4", "PS5", "Xbox One", "Xbox Series X/S"],
-        "studio": "FromSoftware",
-        "director": "Hidetaka Miyazaki"
+        name: "Elden Ring",
+        releaseDate: "2022-02-25",
+        price: 59.99,
+        platforms: ["PC", "PS4", "PS5", "Xbox One", "Xbox Series X/S"],
+        studio: "FromSoftware",
+        director: "Hidetaka Miyazaki"
     },
-    {
-        "name": "Assassin's Creed II",
-        "releaseDate": "2009-11-17",
-        "price": 39.99,
-        "sale": false,
-        "platforms": ["PC", "PS3", "Xbox 360"],
-        "studio": "Ubisoft",
-        "director": "Patrice Désilets"
-    },
-    {
-        "name": "Far Cry 5",
-        "releaseDate": "2018-03-27",
-        "price": 59.99,
-        "sale": false,
-        "platforms": ["PC", "PS4", "Xbox One"],
-        "studio": "Ubisoft",
-        "director": "Dan Hay"
-    },
-    {
-        "name": "Watch Dogs",
-        "releaseDate": "2014-05-27",
-        "price": 49.99,
-        "sale": true,
-        "platforms": ["PC", "PS4", "Xbox One"],
-        "studio": "Ubisoft",
-        "director": "Jonathan Morin"
-    },
-    {
-        "name": "Mario Kart 8 Deluxe",
-        "releaseDate": "2017-04-28",
-        "price": 59.99,
-        "sale": false,
-        "platforms": ["Nintendo Switch"],
-        "studio": "Nintendo",
-        "director": "Kosuke Yabuki"
-    },
-    {
-        "name": "The Legend of Zelda: Breath of the Wild",
-        "releaseDate": "2017-03-03",
-        "price": 59.99,
-        "sale": false,
-        "platforms": ["Nintendo Switch", "Wii U"],
-        "studio": "Nintendo",
-        "director": "Hidemaro Fujibayashi"
-    },
-    {
-        "name": "Super Mario Odyssey",
-        "releaseDate": "2017-10-27",
-        "price": 59.99,
-        "sale": false,
-        "platforms": ["Nintendo Switch"],
-        "studio": "Nintendo",
-        "director": "Kenta Motokura"
-    },
-    {
-        "name": "Red Dead Redemption 2",
-        "releaseDate": "2018-10-26",
-        "price": 59.99,
-        "sale": false,
-        "platforms": ["PC", "PS4", "Xbox One"],
-        "studio": "Rockstar Games",
-        "director": "Rodney E. McCulloch"
-    },
-    {
-        "name": "GTA V",
-        "releaseDate": "2013-09-17",
-        "price": 29.99,
-        "sale": true,
-        "platforms": ["PC", "PS4", "Xbox One", "PS3", "Xbox 360"],
-        "studio": "Rockstar Games",
-        "director": "Leslie Benzies"
-    },
-    {
-        "name": "Max Payne 3",
-        "releaseDate": "2012-05-15",
-        "price": 39.99,
-        "sale": false,
-        "platforms": ["PC", "PS3", "Xbox 360"],
-        "studio": "Rockstar Games",
-        "director": "Sam Houser"
-    },
-    {
-        "name": "L.A. Noire",
-        "releaseDate": "2011-05-17",
-        "price": 49.99,
-        "sale": false,
-        "platforms": ["PC", "PS3", "Xbox 360"],
-        "studio": "Rockstar Games",
-        "director": "Brendan McNamara"
-    },
-    {
-        "name": "Assassin's Creed Odyssey",
-        "releaseDate": "2018-10-05",
-        "price": 59.99,
-        "sale": true,
-        "platforms": ["PC", "PS4", "Xbox One"],
-        "studio": "Ubisoft",
-        "director": "Scott Phillips"
-    },
-    {
-        "name": "Splinter Cell: Blacklist",
-        "releaseDate": "2013-08-20",
-        "price": 39.99,
-        "sale": false,
-        "platforms": ["PC", "PS3", "Xbox 360"],
-        "studio": "Ubisoft",
-        "director": "Patrick Redding"
-    },
-    {
-        "name": "Animal Crossing: New Horizons",
-        "releaseDate": "2020-03-20",
-        "price": 59.99,
-        "sale": true,
-        "platforms": ["Nintendo Switch"],
-        "studio": "Nintendo",
-        "director": "Aya Kyogoku"
-    },
-    {
-        "name": "Metroid Dread",
-        "releaseDate": "2021-10-08",
-        "price": 59.99,
-        "sale": false,
-        "platforms": ["Nintendo Switch"],
-        "studio": "Nintendo",
-        "director": "Yoshio Sakamoto"
-    },
-    {
-        "name": "Assassin's Creed Valhalla",
-        "releaseDate": "2020-11-10",
-        "price": 59.99,
-        "sale": false,
-        "platforms": ["PC", "PS4", "PS5", "Xbox One", "Xbox Series X/S"],
-        "studio": "Ubisoft",
-        "director": "Ashraf Ismail"
-    },
-    {
-        "name": "Nioh",
-        "releaseDate": "2017-02-07",
-        "price": 49.99,
-        "sale": true,
-        "platforms": ["PC", "PS4"],
-        "studio": "Team Ninja",
-        "director": "Fumihiko Yasuda"
-    }
-]
 
-  ];
+    // Ubisoft
+    {
+        name: "Assassin's Creed II",
+        releaseDate: "2009-11-17",
+        price: 39.99,
+        platforms: ["PC", "PS3", "Xbox 360"],
+        studio: "Ubisoft",
+        director: "Patrice Désilets"
+    },
+    {
+        name: "Far Cry 5",
+        releaseDate: "2018-03-27",
+        price: 59.99,
+        platforms: ["PC", "PS4", "Xbox One"],
+        studio: "Ubisoft",
+        director: "Dan Hay"
+    },
+    {
+        name: "Watch Dogs",
+        releaseDate: "2014-05-27",
+        price: 49.99,
+        platforms: ["PC", "PS4", "Xbox One"],
+        studio: "Ubisoft",
+        director: "Jonathan Morin"
+    },
+    {
+        name: "Assassin's Creed Odyssey",
+        releaseDate: "2018-10-05",
+        price: 59.99,
+        platforms: ["PC", "PS4", "Xbox One"],
+        studio: "Ubisoft",
+        director: "Scott Phillips"
+    },
+    {
+        name: "Splinter Cell: Blacklist",
+        releaseDate: "2013-08-20",
+        price: 39.99,
+        platforms: ["PC", "PS3", "Xbox 360"],
+        studio: "Ubisoft",
+        director: "Patrick Redding"
+    },
+    {
+        name: "Assassin's Creed Valhalla",
+        releaseDate: "2020-11-10",
+        price: 59.99,
+        platforms: ["PC", "PS4", "PS5", "Xbox One", "Xbox Series X/S"],
+        studio: "Ubisoft",
+        director: "Ashraf Ismail"
+    },
+
+    // Rockstar Games
+    {
+        name: "Red Dead Redemption 2",
+        releaseDate: "2018-10-26",
+        price: 59.99,
+        platforms: ["PC", "PS4", "Xbox One"],
+        studio: "Rockstar Games",
+        director: "Rodney E. McCulloch"
+    },
+    {
+        name: "GTA V",
+        releaseDate: "2013-09-17",
+        price: 29.99,
+        platforms: ["PC", "PS4", "Xbox One", "PS3", "Xbox 360"],
+        studio: "Rockstar Games",
+        director: "Leslie Benzies"
+    },
+    {
+        name: "Max Payne 3",
+        releaseDate: "2012-05-15",
+        price: 39.99,
+        platforms: ["PC", "PS3", "Xbox 360"],
+        studio: "Rockstar Games",
+        director: "Sam Houser"
+    },
+    {
+        name: "L.A. Noire",
+        releaseDate: "2011-05-17",
+        price: 49.99,
+        platforms: ["PC", "PS3", "Xbox 360"],
+        studio: "Rockstar Games",
+        director: "Brendan McNamara"
+    },
+
+    // Capcom Production Studio 4
+    {
+        name: "Resident Evil 4",
+        releaseDate: "2005-01-11",
+        price: 39.99,
+        platforms: ["PC", "PS2", "GameCube"],
+        studio: "Capcom Production Studio 4",
+        director: "Shinji Mikami"
+    },
+    {
+        name: "Devil May Cry",
+        releaseDate: "2001-08-23",
+        price: 29.99,
+        platforms: ["PS2"],
+        studio: "Capcom Production Studio 4",
+        director: "Hideki Kamiya"
+    },
+    {
+        name: "Resident Evil 2",
+        releaseDate: "1998-01-21",
+        price: 39.99,
+        platforms: ["PC", "PS1", "N64"],
+        studio: "Capcom Production Studio 4",
+        director: "Hideki Kamiya"
+    },
+    {
+        name: "Resident Evil 3: Nemesis",
+        releaseDate: "1999-09-22",
+        price: 39.99,
+        platforms: ["PC", "PS1", "Dreamcast"],
+        studio: "Capcom Production Studio 4",
+        director: "Kazuhiro Aoyama"
+    },
+
+    // CD Projekt Red
+    {
+        name: "The Witcher 3: Wild Hunt",
+        releaseDate: "2015-05-19",
+        price: 49.99,
+        platforms: ["PC", "PS4", "Xbox One", "Nintendo Switch"],
+        studio: "CD Projekt Red",
+        director: "Konrad Tomaszkiewicz"
+    },
+    {
+        name: "Cyberpunk 2077",
+        releaseDate: "2020-12-10",
+        price: 59.99,
+        platforms: ["PC", "PS4", "PS5", "Xbox One", "Xbox Series X/S"],
+        studio: "CD Projekt Red",
+        director: "Adam Badowski"
+    },
+
+    // Team Cherry
+    {
+        name: "Hollow Knight",
+        releaseDate: "2017-02-24",
+        price: 14.99,
+        platforms: ["PC", "Nintendo Switch", "PS4", "Xbox One"],
+        studio: "Team Cherry",
+        director: "Ari Gibson"
+    },
+
+    // Additional Games
+    {
+        name: "Resident Evil",
+        releaseDate: "1996-03-22",
+        price: 39.99,
+        platforms: ["PC", "PS1"],
+        studio: "Capcom Production Studio 4",
+        director: "Shinji Mikami"
+    },
+    {
+        name: "Resident Evil Code: Veronica",
+        releaseDate: "2000-02-03",
+        price: 39.99,
+        platforms: ["PC", "Dreamcast", "PS2"],
+        studio: "Capcom Production Studio 4",
+        director: "Shinji Mikami"
+    },
+    {
+        name: "Resident Evil 5",
+        releaseDate: "2009-03-05",
+        price: 29.99,
+        platforms: ["PC", "PS3", "Xbox 360"],
+        studio: "Capcom Production Studio 4",
+        director: "Kenichi Ueda"
+    },
+    {
+        name: "Assassin's Creed III",
+        releaseDate: "2012-10-30",
+        price: 39.99,
+        platforms: ["PC", "PS3", "Xbox 360", "Wii U"],
+        studio: "Ubisoft",
+        director: "Alex Hutchinson"
+    },
+    {
+        name: "The Witcher 2: Assassins of Kings",
+        releaseDate: "2011-05-17",
+        price: 29.99,
+        platforms: ["PC", "Xbox 360"],
+        studio: "CD Projekt Red",
+        director: "Adam Badowski"
+    }
+];
+
   
 
--- app/JS/main.js
+++ app/JS/main.js
@@ -1,5 +1,82 @@
 import "../CSS/style.css";
 import {games} from "./items";
 
-games.forEach((game) => game.forEach((gamer) => console.log(gamer["name"])))
+games.forEach((game) => console.log(game.name))
 
+
+const DOM ={
+    cardbox: document.querySelector(".cardbox"),
+    fromsoftbtn: document.getElementById("FromSoftware"),
+    ubisoftbtn: document.getElementById("Ubisoft"),
+    rockstarbtn: document.getElementById("RockstarGames"),
+    capcombtn: document.getElementById("Capcom"),
+    cdprobtn: document.getElementById("CDProject"),
+    teamcherbtn: document.getElementById("TeamCherry"),
+}
+
+
+function createcard(game){
+    game.forEach((gamename) => DOM.cardbox.insertAdjacentHTML("beforeend", 
+        `<div class = "card"> 
+        <h1 class = "name">${gamename.name}</h1>
+        <h5 class = "price">${gamename.price}</h5>
+        <h5 class = "studio" id = "${gamename.studio}">${gamename.studio}</h5>
+        <br>
+    </div>`))
+    
+}
+createcard(games)
+
+function filterstudio(studio){
+
+    const cards = document.querySelectorAll(".card")
+
+    cards.forEach((card) => {
+
+        const gamestudio = card.querySelector(".studio").getAttribute("id");
+
+        if (gamestudio != studio){
+            card.remove()
+        }
+        
+
+    })
+
+}
+
+function fromsoftfilter(){
+    const frombutton = DOM.fromsoftbtn
+    frombutton.addEventListener("click", () => filterstudio("FromSoftware"))
+}
+fromsoftfilter()
+ 
+function ubisoftfilter(){
+    const ubibutton = DOM.ubisoftbtn
+    ubibutton.addEventListener("click", () => filterstudio("Ubisoft"))
+}
+ubisoftfilter()
+
+function rockstarfilter(){
+    const rockstarbutton = DOM.rockstarbtn
+    rockstarbutton.addEventListener("click", () => filterstudio("Rockstar Games"))
+}
+rockstarfilter()
+ 
+function capcomfilter(){
+    const capcombutton = DOM.capcombtn
+    capcombutton.addEventListener("click", () => filterstudio("Capcom Production Studio 4"))
+}
+capcomfilter()
+ 
+function cdprojectfilter(){
+    const cdprobutton = DOM.cdprobtn
+    cdprobutton.addEventListener("click", () => filterstudio("CD Projekt Red"))
+}
+cdprojectfilter()
+ 
+function teamchefilter(){
+    const teamchebutton = DOM.teamcherbtn
+    teamchebutton.addEventListener("click", () => filterstudio("Team Cherry"))
+}
+teamchefilter()
+ 
