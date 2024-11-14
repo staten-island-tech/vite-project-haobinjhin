99bd5ee148a0bd4390d4079524212124820b4d3f
haobinjhin <144480283+haobinjhin@users.noreply.github.com>
2024-11-14 12:17:09 -0500

    dfsfsfsddfsdfs

--- app/JS/items.js
+++ app/JS/items.js
@@ -7,7 +7,7 @@ export const games = [
         platforms: ["PC", "PS3", "Xbox 360"],
         studio: "FromSoftware",
         director: "Hidetaka Miyazaki",
-        pic: "./PIC/images/DS1.png",
+        pic: "",
     },
     {
         name: "Bloodborne",
